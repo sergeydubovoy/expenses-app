5518b222c407838c4273f14e5eaec84470e05a47
Sergey Dubovoy <sergeydubovoi@gmail.com>
2023-05-09 23:42:03 +0300

    Added and refactored JS code, added HTML for new layout

---
+++ js/index.js
@@ -0,0 +1,100 @@
+const LIMIT = 10000;
+const CURRENCY = "₽";
+const STATUS_IN_LIMIT = "Все хорошо";
+const STATUS_OUT_OF_LIMIT = "Все не очень";
+const STATUS_OUT_OF_LIMIT_CLASSNAME = "status_red";
+
+const inputNode = document.querySelector('[js-data="js-input"]');
+const buttonNode = document.querySelector('[js-data="js-button-add-expense"]');
+const historyNode = document.querySelector('[js-data="js-history"]');
+const sumNode = document.querySelector('[data-js="js-sum"]');
+const limitNode = document.querySelector('[data-js="js-limit"]');
+const statusNode = document.querySelector('[data-js="js-status"]');
+
+const expenses = [];
+
+initApp(expenses);
+
+buttonNode.addEventListener("click", function () {
+  const expense = getExpenseFromUser();
+
+  if (!expense) {
+    return;
+  }
+
+  trackExspenses(expense);
+  render(expenses);
+});
+
+function initApp(expenses) {
+  limitNode.innerText = LIMIT;
+  statusNode.innerText = STATUS_IN_LIMIT;
+  sumNode.innerText = calculateExpenses(expenses);
+}
+
+function trackExspenses(expense) {
+  expenses.push(expense);
+}
+
+function getExpenseFromUser() {
+  if (!inputNode.value) {
+    return null;
+  }
+
+  const expense = parseInt(inputNode.value);
+
+  clearInput();
+
+  return expense;
+}
+
+function clearInput() {
+  inputNode.value = "";
+}
+
+function calculateExpenses(expenses) {
+  let sum = 0;
+
+  expenses.forEach((element) => {
+    sum += element;
+  });
+
+  return sum;
+}
+
+function render(expenses) {
+  const sum = calculateExpenses(expenses);
+
+  renderHistory(expenses);
+  renderSum(sum);
+  renderStatus(sum);
+}
+
+function renderHistory(expenses) {
+  let expensesListHTML = "";
+
+  expenses.forEach((element) => {
+    expensesListHTML += `<li>${element} ${CURRENCY}</li>`;
+  });
+
+  historyNode.innerHTML = `<ol>${expensesListHTML}</ol>`;
+}
+
+function renderSum(sum) {
+  sumNode.innerText = sum;
+}
+
+function renderStatus(sum) {
+  if (sum <= LIMIT) {
+    statusNode.innerText = STATUS_IN_LIMIT;
+  } else {
+    statusNode.innerText = STATUS_OUT_OF_LIMIT;
+    statusNode.classList.add(STATUS_OUT_OF_LIMIT_CLASSNAME);
+  }
+}
+
+inputNode.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    buttonNode.click();
+  }
+});
