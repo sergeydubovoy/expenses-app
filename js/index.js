1280573751ce3c4719199be58aab09bfb2bec767
Sergey Dubovoy <sergeydubovoi@gmail.com>
2023-05-15 21:47:13 +0300

    Fixed bugs and refactoring code

--- js/index.js
+++ js/index.js
@@ -1,83 +1,53 @@
+// Элементы со значениями
+
 let LIMIT = 10000;
-
-// Константы со значениями
-
-const CURRENCY = "₽";
+const CURRENCY = " ₽";
 const STATUS_IN_LIMIT = "Все хорошо";
 const STATUS_OUT_OF_LIMIT = "Лимит превышен";
 const STATUS_OUT_OF_LIMIT_CLASSNAME = "status_red";
 
-// Константы для добавления расходов
+// Элементы для добавления расходов, статистики, выбора категорий
 
-const inputNode = document.querySelector('[js-data="expense__input"]');
-const buttonAddExpense = document.querySelector(
-  '[js-data="expense__button_add"]'
-);
-const historyNode = document.querySelector('[js-data="history"]');
-const sumNode = document.querySelector('[js-data="sum"]');
-const limitNode = document.querySelector('[js-data="limit__value"]');
-const statusNode = document.querySelector('[js-data="status"]');
-const buttonResetHistory = document.querySelector(
-  '[js-data="history__button_reset"]'
-);
+const inputNode = document.getElementById("expenseInput");
+const addExpenseButton = document.getElementById("addExpenseButton");
+const categorySelect = document.getElementById("categorySelect");
+let categoryNode = categorySelect.value;
+const sumNode = document.getElementById("sumValue");
+const limitNode = document.getElementById("limitValue");
+const statusNode = document.getElementById("status");
+const historyNode = document.getElementById("history");
+const resetHistoryButton = document.getElementById("resetHistoryButton");
 
 const expenses = [];
 
-// Константы для выбора категория
-
-const categoriesModalWrapper = document.querySelector(
-  '[js-data="categories__modal-wrapper"]'
-);
-const buttonChoseCategory = document.querySelector(
-  '[js-data="categories__button"]'
-);
-const categoryNode = document.querySelector('[js-data="categories__item"]');
-const categoryNodes = document.querySelectorAll('[js-data="categories__item"]');
-const arrowButton = document.querySelector('[js-data="button__span"]');
-
 let selectedCategory = "";
 
-// Константы для вызова попапа и изменения лимита
+// Элементы для вызова попапа и изменения лимита
 
 const body = document.querySelector(".body");
-const openPopupButton = document.querySelector('[js-data="limit__button"]');
-const closePopupButton = document.querySelector(
-  '[js-data="popup__close-button"]'
-);
-const changeLimitInput = document.querySelector(
-  '[js-data="change-limit__input"]'
-);
-const changeLimitButton = document.querySelector(
-  '[js-data="change-limit__button"]'
-);
-const popup = document.querySelector('[js-data="popup"]');
+const popup = document.getElementById("popup");
+const openPopupButton = document.getElementById("openPopupButton");
+const closePopupButton = document.getElementById("closePopupButton");
+const newLimitInput = document.getElementById("newLimitInput");
+const changeLimitButton = document.getElementById("changeLimitButton");
 
-initApp(expenses);
+const calculateExpenses = (expenses) => {
+  const total = expenses.reduce((sum, expense) => {
+    if (typeof expense.amount === "number") {
+      return sum + expense.amount;
+    }
+    return sum;
+  }, 0);
 
-buttonAddExpense.addEventListener("click", function () {
-  const expense = getExpenseFromUser();
+  return total;
+};
 
-  if (!expense) {
-    return;
-  }
-
-  trackExspenses(expense);
-  render(expenses);
-  resetSelectedCategory();
-});
-
-function initApp(expenses) {
-  limitNode.innerText = LIMIT;
-  statusNode.innerText = STATUS_IN_LIMIT;
-  sumNode.innerText = calculateExpenses(expenses);
-}
-
-function trackExspenses(expense) {
-  const expenseObject = { name: expense, category: selectedCategory };
+const trackExpenses = (expense) => {
+  const expenseObject = { amount: expense, category: categorySelect.value };
   expenses.push(expenseObject);
-}
+};
 
-function getExpenseFromUser() {
+const getExpenseFromUser = () => {
   if (!inputNode.value) {
     return null;
   }
@@ -87,136 +57,149 @@ function getExpenseFromUser() {
   clearInput();
 
   return expense;
-}
+};
 
-function clearInput() {
+const initApp = (expenses) => {
+  limitNode.innerText = LIMIT + CURRENCY;
+  statusNode.innerText = STATUS_IN_LIMIT;
+  sumNode.innerText = calculateExpenses(expenses) + CURRENCY;
+};
+
+initApp(expenses);
+
+addExpenseButton.addEventListener("click", () => {
+  const expense = getExpenseFromUser();
+
+  if (!expense) {
+    return;
+  }
+
+  trackExpenses(expense);
+  render(expenses);
+  resetSelectedCategory();
+});
+
+const clearInput = () => {
   inputNode.value = "";
-}
+};
 
-function calculateExpenses(expenses) {
-  const total = expenses.reduce((sum, expense) => {
-    if (typeof expense.name === "number") {
-      return sum + expense.name;
-    }
-    return sum;
-  }, 0);
+const resetSelectedCategory = () => {
+  categorySelect.selectedIndex = 0;
+};
 
-  return total;
-}
-
-function resetSelectedCategory() {
-  selectedCategory = "";
-  buttonChoseCategory.textContent = "Категория";
-}
-
-function render(expenses) {
+const render = (expenses) => {
   const sum = calculateExpenses(expenses);
 
   renderHistory(expenses);
   renderSum(sum);
   renderStatus(sum);
-}
+};
 
-function renderHistory(expenses) {
+const renderHistory = (expenses) => {
   let expensesListHTML = "";
 
   expenses.forEach((expense) => {
-    expensesListHTML += `<li>${expense.name} ${CURRENCY} - ${
+    expensesListHTML += `<li>${expense.amount} ${CURRENCY} - ${
       expense.category ? expense.category : ""
     }</li>`;
   });
 
   historyNode.innerHTML = `<ol class="history__list">${expensesListHTML}</ol>`;
-}
+};
 
-function renderSum(sum) {
-  sumNode.innerText = sum;
-}
+const renderSum = (sum) => {
+  sumNode.innerText = sum + CURRENCY;
+};
 
-function renderStatus(sum) {
+const renderStatus = (sum) => {
   if (sum <= LIMIT) {
     statusNode.innerText = STATUS_IN_LIMIT;
   } else {
-    statusNode.innerText = STATUS_OUT_OF_LIMIT;
+    statusNode.innerText = `${STATUS_OUT_OF_LIMIT} (${
+      LIMIT - sum
+    } ${CURRENCY})`;
     statusNode.classList.add(STATUS_OUT_OF_LIMIT_CLASSNAME);
   }
-}
+};
 
 // Добавление расхода по нажатию Enter
 
-inputNode.addEventListener("keydown", function (event) {
+inputNode.addEventListener("keydown", (event) => {
   if (event.key === "Enter") {
-    buttonAddExpense.click();
+    addExpenseButton.click();
   }
 });
 
 // Выбор категорий
 
-buttonChoseCategory.addEventListener("click", function () {
-  toggleCategoriesModal();
-  arrowButton.style.transform =
-    arrowButton.style.transform === "rotateZ(90deg)"
-      ? "rotateZ(0deg)"
-      : "rotateZ(90deg)";
+categorySelect.addEventListener("change", () => {
+  categoryNode = categorySelect.value;
 });
 
-function selectCategory(category) {
-  selectedCategory = category;
-  categoriesModalWrapper.classList.remove("categories__modal-wrapper_active");
-  buttonChoseCategory.textContent = selectedCategory;
-  arrowButton.style.transform = "rotateZ(0deg)";
-}
+// Открытие попапа
 
-categoryNodes.forEach(function (categoryNode) {
-  categoryNode.addEventListener("click", function () {
-    selectCategory(categoryNode.textContent);
-    arrowButton.style.transform = "rotateZ(0deg)";
-  });
-});
-
-function toggleCategoriesModal() {
-  categoriesModalWrapper.classList.toggle("categories__modal-wrapper_active");
-  selectedCategory = "";
-  arrowButton.style.transform = "rotateZ(0deg)";
-}
-
-// Попап
-
-openPopupButton.addEventListener("click", function () {
+openPopupButton.addEventListener("click", () => {
   popup.style.opacity = "1";
   popup.style.visibility = "visible";
   body.classList.toggle("body_fixed");
 });
 
-changeLimitButton.addEventListener("click", function (event) {
-  const newLimitValue = changeLimitInput.value;
+// Функция изменения лимита, в которой:
+// - присвоение значения нового лимита от инпута
+// - проверка на пустую строку и значение меньшее или равное нулю
+// - обновление лимита
+// - пересчет лимита и тоггл класса, чтобы статус правильно отображался
+// - закрытие попапа
+
+changeLimitButton.addEventListener("click", (event) => {
+  const newLimitValue = newLimitInput.value;
+  if (!newLimitValue || Number(newLimitValue) <= 0) {
+    alert("Лимит должен быть числом, большим нуля!");
+    return;
+  }
   LIMIT = newLimitValue;
   limitNode.innerHTML = LIMIT;
-
-  popup.style.opacity = "0";
-  popup.style.visibility = "hidden";
-  body.classList.remove("body_fixed");
-});
-
-closePopupButton.addEventListener("click", function () {
+  renderStatus(calculateExpenses(expenses));
+  statusNode.classList.toggle(
+    STATUS_OUT_OF_LIMIT_CLASSNAME,
+    calculateExpenses(expenses) > LIMIT
+  );
   closePopup();
 });
 
-popup.addEventListener("click", function (event) {
+// Вызываем обработчик событий, чтобы лимит менялся еще и по нажатию Enter
+
+newLimitInput.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    changeLimitButton.click();
+  }
+});
+
+// По клику на кнопку закрытия попапа, он закрывается
+
+closePopupButton.addEventListener("click", () => {
+  closePopup();
+});
+
+// Проверяем был ли клик мышкой на сером фоне попапа, если да, то закрываем попап
+
+popup.addEventListener("click", (event) => {
   if (event.target === popup) {
     closePopup();
   }
 });
 
-function closePopup() {
+// Закрываем попап
+
+const closePopup = () => {
   popup.style.opacity = "0";
   popup.style.visibility = "hidden";
   body.classList.remove("body_fixed");
-}
+};
 
 // Сброс
 
-buttonResetHistory.addEventListener("click", function () {
+resetHistoryButton.addEventListener("click", () => {
   expenses.length = 0;
   render([]);
   resetSelectedCategory();
